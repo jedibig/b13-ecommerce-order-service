14dba387b28d113deb914ba94f0b5335c27b68e9
jedidiahbowo <jedidiahbowo@yahoo.com>
2020-05-15 16:19:53 -0500

    update controller and add sonar plugin

--- src/main/java/com/b13/orderservice/controller/OrderController.java
+++ src/main/java/com/b13/orderservice/controller/OrderController.java
@@ -1,11 +1,24 @@
 package com.b13.orderservice.controller;
 
+import com.b13.orderservice.dto.Order;
+import com.b13.orderservice.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/orders")
+@AllArgsConstructor
 public class OrderController {
 
+    final OrderService orderService;
 
+    @GetMapping("/{id}")
+    public Optional<Order> retrieveOrderById(@PathVariable("id") long id){
+        return orderService.getOrderById(id);
+    }
 }
